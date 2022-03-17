91aa941696b793d6cc2499e02ab7a4066996a9c0
Ejay02 <e.jae02@gmail.com>
2022-03-17 03:35:11 +0100

    Infinite scroll

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <h1>My Anime Feed</h1>
+  </header>
+  <main>
+    <post v-for="(anime, i) in anime_list" :key="i" :anime="anime" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Post from "./components/Post.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Post },
+
+  data() {
+    return {
+      anime_list: [],
+    };
+  },
+
+  methods: {
+    getAnime() {
+      const anime_titles = [
+        "Naruto",
+        "Demon Slayer",
+        "Dragon Ball",
+        "My Hero Academia",
+        "Sword Art Online",
+        "Tokyo Ghoul",
+        "Darling in the Franxx",
+        "Code Geass",
+        "One Piece",
+        "Fairy Tail",
+        "Bleach",
+        "Attack on Titan",
+        "Hunter x Hunter",
+      ];
+
+      const anime = [];
+
+      for (let i = 0; i < 10; i++) {
+        anime.push({
+          title: anime_titles[Math.floor(Math.random() * anime_titles.length)],
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        });
+      }
+
+      return anime;
+    },
+    handleScroll() {
+      if (
+        window.scrollY + window.innerHeight >=
+        document.body.scrollHeight - 50
+      ) {
+        this.anime_list = [...this.anime_list, ...this.getAnime()];
+      }
+    },
+  },
+  mounted() {
+    this.anime_list = this.getAnime();
+    window.addEventListener("scroll", this.handleScroll);
+  },
+};
 </script>
 
 <style>
-#app {
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  /* background-color: #2c3e50; */
+  background-color: #2f2f2f;
+  /* margin-top: 60px; */
+  min-height: 100vh;
+  padding-top: 3rem;
+  /* color: azure; */
+  color: #ffcb74;
+}
+
+header h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+header {
+  margin-bottom: 2rem;
+}
+
+main {
+  padding: 0 2rem;
+  min-width: 640px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,29 @@
+<template>
+  <article>
+    <h2>{{ anime.title }}</h2>
+    <p>
+      {{ anime.description }}
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ["anime"],
+};
+</script>
+
+<style>
+article {
+  background-color: #f6f6f6;
+  border-radius: 1rem;
+  padding: 1rem 2rem;
+  box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+  margin-bottom: 2rem;
+  color: #2f2f2f;
+}
+
+article h2 {
+  margin-bottom: 1rem;
+}
+</style>
